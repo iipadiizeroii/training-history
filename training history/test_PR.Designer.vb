387a4258664714a0c68dd513bd5f127b3cbb2910
iipadiizeroii <56190642+iipadiizeroii@users.noreply.github.com>
2019-10-31 22:15:15 +0700

    ืทดสอบทำ from ค่าใช้จ่าย

--- training history/test_PR.Designer.vb
+++ training history/test_PR.Designer.vb
@@ -22,22 +22,16 @@ Partial Class test_PR
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.Date_training1 = New System.Windows.Forms.DateTimePicker()
+        Me.Date_training2 = New System.Windows.Forms.DateTimePicker()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(102, 14)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(260, 20)
-        Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(378, 12)
+        Me.Button1.Location = New System.Drawing.Point(527, 10)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -60,26 +54,47 @@ Partial Class test_PR
         Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
         Me.CrystalReportViewer1.Location = New System.Drawing.Point(18, 49)
         Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
-        Me.CrystalReportViewer1.Size = New System.Drawing.Size(867, 318)
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(972, 525)
         Me.CrystalReportViewer1.TabIndex = 3
+        Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+        '
+        'Date_training1
+        '
+        Me.Date_training1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.Date_training1.Location = New System.Drawing.Point(87, 13)
+        Me.Date_training1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Date_training1.Name = "Date_training1"
+        Me.Date_training1.Size = New System.Drawing.Size(165, 20)
+        Me.Date_training1.TabIndex = 5
+        '
+        'Date_training2
+        '
+        Me.Date_training2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.Date_training2.Location = New System.Drawing.Point(293, 13)
+        Me.Date_training2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Date_training2.Name = "Date_training2"
+        Me.Date_training2.Size = New System.Drawing.Size(165, 20)
+        Me.Date_training2.TabIndex = 6
         '
         'test_PR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(897, 379)
+        Me.ClientSize = New System.Drawing.Size(1002, 586)
+        Me.Controls.Add(Me.Date_training2)
+        Me.Controls.Add(Me.Date_training1)
         Me.Controls.Add(Me.CrystalReportViewer1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
         Me.Name = "test_PR"
         Me.Text = "test_PR"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Date_training1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Date_training2 As System.Windows.Forms.DateTimePicker
 End Class
--- training history/test_PR.vb
+++ training history/test_PR.vb
@@ -6,15 +6,30 @@ Public Class test_PR
 
 
         Dim rpt As New ReportDocument()
+        Dim rpt2 As New ReportDocument()
         Dim directory As String = My.Application.Info.DirectoryPath
         'rpt.Load(directory & "\myCrystalReport1.rpt")
-        rpt.Load("G:\โปรเจค\training history\training-history\training history\PR_Training.rpt")
-        rpt.SetParameterValue("trainingInid1", Me.TextBox1.Text)
+        rpt.Load("C:\Users\Duck\Desktop\training-history\training history\PR_Expenses_in.rpt")
+        rpt.SetParameterValue("trainingdate1", Me.Date_training1.Text)
+        rpt.SetParameterValue("trainingdate2", Me.Date_training2.Text)
+
         Me.CrystalReportViewer1.ReportSource = rpt
+        Me.CrystalReportViewer1.ReportSource = rpt2
         Me.CrystalReportViewer1.Refresh()
+
+
 
 
     End Sub
 
 
+    Private Sub test_PR_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.WindowState = FormWindowState.Normal
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
 End Class
