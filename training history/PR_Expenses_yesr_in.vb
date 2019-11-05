708662f3f245c9e0190e554e9688c525d7189dbe
iipadiizeroii <56190642+iipadiizeroii@users.noreply.github.com>
2019-11-05 23:26:04 +0700

    ทำ RP PO การจัดอบรม

--- training history/PR_Expenses_yesr_in.vb
+++ training history/PR_Expenses_yesr_in.vb
@@ -18,7 +18,7 @@ Imports System
 Imports System.ComponentModel
 
 
-Public Class RP_PO_internal_training
+Public Class PR_Expenses_yesr_in
     Inherits ReportClass
     
     Public Sub New()
@@ -27,7 +27,7 @@ Public Class RP_PO_internal_training
     
     Public Overrides Property ResourceName() As String
         Get
-            Return "RP_PO_internal_training.rpt"
+            Return "PR_Expenses_yesr_in.rpt"
         End Get
         Set
             'Do nothing
@@ -45,7 +45,7 @@ Public Class RP_PO_internal_training
     
     Public Overrides Property FullResourceName() As String
         Get
-            Return "training_history.RP_PO_internal_training.rpt"
+            Return "training_history.PR_Expenses_yesr_in.rpt"
         End Get
         Set
             'Do nothing
@@ -86,39 +86,15 @@ Public Class RP_PO_internal_training
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property ReportFooterSection1() As CrystalDecisions.CrystalReports.Engine.Section
-        Get
-            Return Me.ReportDefinition.Sections(4)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property ReportFooterSection2() As CrystalDecisions.CrystalReports.Engine.Section
-        Get
-            Return Me.ReportDefinition.Sections(5)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
     Public ReadOnly Property Section5() As CrystalDecisions.CrystalReports.Engine.Section
         Get
-            Return Me.ReportDefinition.Sections(6)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Parameter_PR_traingin_id() As CrystalDecisions.[Shared].IParameterField
-        Get
-            Return Me.DataDefinition.ParameterFields(0)
+            Return Me.ReportDefinition.Sections(4)
         End Get
     End Property
 End Class
 
 <System.Drawing.ToolboxBitmapAttribute(GetType(CrystalDecisions.[Shared].ExportOptions), "report.bmp")>  _
-Public Class CachedRP_PO_internal_training
+Public Class CachedPR_Expenses_yesr_in
     Inherits Component
     Implements ICachedReport
     
@@ -160,7 +136,7 @@ Public Class CachedRP_PO_internal_training
     End Property
     
     Public Overridable Function CreateReport() As CrystalDecisions.CrystalReports.Engine.ReportDocument Implements CrystalDecisions.ReportSource.ICachedReport.CreateReport
-        Dim rpt As RP_PO_internal_training = New RP_PO_internal_training()
+        Dim rpt As PR_Expenses_yesr_in = New PR_Expenses_yesr_in()
         rpt.Site = Me.Site
         Return rpt
     End Function
