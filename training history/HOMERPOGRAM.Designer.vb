de89c7f2afb6dd5146a9cf39fb1d3682dc9d8aa4
iipadiizeroii <56190642+iipadiizeroii@users.noreply.github.com>
2019-11-07 11:49:54 +0700

    แต่งหน้าจอ

--- training history/HOMERPOGRAM.Designer.vb
+++ training history/HOMERPOGRAM.Designer.vb
@@ -23,7 +23,6 @@ Partial Class HOMERPOGRAM
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
@@ -31,34 +30,26 @@ Partial Class HOMERPOGRAM
         Me.Button6 = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.PaleGreen
+        Me.Label1.BackColor = System.Drawing.Color.Aquamarine
         Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label1.Cursor = System.Windows.Forms.Cursors.Default
         Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Label1.Font = New System.Drawing.Font("TH SarabunPSK", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Location = New System.Drawing.Point(21, 26)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(546, 66)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "หน้าแรกโปรแกรมบันทึกข้อมูลการอบรม"
-        '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("TH SarabunPSK", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
-        Me.Button1.Location = New System.Drawing.Point(6, 19)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(178, 48)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "บันทึกข้อมูลพนักงาน"
-        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
@@ -68,7 +59,7 @@ Partial Class HOMERPOGRAM
         Me.Button2.Size = New System.Drawing.Size(178, 48)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "บันทึกข้อมูลวิทยากร"
-        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
@@ -79,7 +70,7 @@ Partial Class HOMERPOGRAM
         Me.Button3.Size = New System.Drawing.Size(178, 48)
         Me.Button3.TabIndex = 1
         Me.Button3.Text = "เพิ่มหลักสูตรการอบรม"
-        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
@@ -90,7 +81,7 @@ Partial Class HOMERPOGRAM
         Me.Button4.Size = New System.Drawing.Size(178, 48)
         Me.Button4.TabIndex = 1
         Me.Button4.Text = "จัดอบรมภายใน"
-        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button5
@@ -101,7 +92,7 @@ Partial Class HOMERPOGRAM
         Me.Button5.Size = New System.Drawing.Size(178, 48)
         Me.Button5.TabIndex = 1
         Me.Button5.Text = "จัดอบรมภายนอก"
-        Me.Button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button6
@@ -112,7 +103,7 @@ Partial Class HOMERPOGRAM
         Me.Button6.Size = New System.Drawing.Size(178, 48)
         Me.Button6.TabIndex = 1
         Me.Button6.Text = "พิมพ์รายงาน"
-        Me.Button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button6.UseVisualStyleBackColor = True
         '
         'GroupBox1
@@ -124,36 +115,56 @@ Partial Class HOMERPOGRAM
         Me.GroupBox1.Controls.Add(Me.Button5)
         Me.GroupBox1.Controls.Add(Me.Button3)
         Me.GroupBox1.Controls.Add(Me.Button4)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 78)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 105)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(194, 358)
         Me.GroupBox1.TabIndex = 2
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "เมนูโปรแกรม"
         '
         'GroupBox2
         '
-        Me.GroupBox2.Location = New System.Drawing.Point(214, 78)
+        Me.GroupBox2.Location = New System.Drawing.Point(221, 106)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(663, 357)
+        Me.GroupBox2.Size = New System.Drawing.Size(699, 357)
         Me.GroupBox2.TabIndex = 3
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.GroupBox2)
+        Me.GroupBox3.Controls.Add(Me.GroupBox1)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(940, 485)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("TH SarabunPSK", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(178, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "บันทึกข้อมูลพนักงาน"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'HOMERPOGRAM
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DodgerBlue
-        Me.ClientSize = New System.Drawing.Size(895, 451)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(963, 503)
+        Me.Controls.Add(Me.GroupBox3)
         Me.Name = "HOMERPOGRAM"
         Me.Text = "HOMERPOGRAM"
         Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
@@ -165,4 +176,5 @@ Partial Class HOMERPOGRAM
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
     Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
 End Class
